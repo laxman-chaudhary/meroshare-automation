52396d460aa64ec1eb66acd0c3ea893be10f24f9
laxman-chaudhary <peerless.peter@gmail.com>
2023-08-23 17:43:12 +0545

    Version 2

--- alex/bot/telegram.sh
+++ alex/bot/telegram.sh
@@ -14,18 +14,36 @@ while IFS= read -r line; do
         email=$(echo "$line" | sed -nE "s/.*'email': '([^']+)'.*/\1/p")
         date=$(echo "$line" | sed -nE "s/.*'applied_time': '([^']+)'.*/\1/p")
 
-# Prepare the message with proper formatting (using Markdown for bold)
+	elif [[ $line =~ ^[0-9]+$ ]]; then
+        # Extract "Applied Kitta" value if the line contains only digits
+        applied_kitta="$line"
+    fi
+
+   # Check if all the necessary information has been collected
+    if [[ -n "$share_type" && -n "$share_group" && -n "$company" && -n "$email" && -n "$date" && -n "$applied_kitta" ]]; then
+        # Prepare the message with proper formatting (using Markdown for bold)
    message="Dear *$user_name*,
-   Share Applied Sucessfully
+   Share Applied Sucessfully: *$applied_kitta किट्टा*
+
    *Type:*  \`$share_type\`
    *Group:* \`$share_group\`
-   *Company Name:* \`$company\` 
+   *Applied Kitta:* \`$applied_kitta\`
    *Email:* \`$email\`
    *Applied Date:* \`$date\`
+   *Company Name:* \`$company\`
 
    _*Please maintain minimum banalce in your Account_"
         
         # Send the message using curl
         curl -s -X POST "https://api.telegram.org/bot$secret_key/sendMessage" -d "chat_id=$chat_id" -d "text=$message" -d "parse_mode=Markdown"
+   
+	# Reset variables for the next entry
+        share_type=""
+        share_group=""
+        company=""
+        email=""
+        date=""
+        applied_kitta=""
+
     fi
 done < ../meroshare.log
